2ec8129431a239d05ee9d8c0faa2c692d95e49a0
Illia Kuzmenko <illia.kuzmenko@icloud.com>
2021-11-13 14:34:04 +0200

    Update MarkovChains.py
    Variable refactoring

--- MarkovChains.py
+++ MarkovChains.py
@@ -4,27 +4,28 @@ from graphviz import Source
 
 class MarkovChains:
 
-    def __init__(self, initial_distribution, stochastic_matrix):
+    def __init__(self, b0, P):
 
-        self.B = np.array(initial_distribution)
-        self.P = np.array(stochastic_matrix)
+        self.initial_distribution = np.array(b0)
+        self.transition_matrix = np.array(P)
 
-    def chains(self):
+    def ergodic_distribution(self):
 
-        b = self.B
+        b = self.initial_distribution
 
         for k in range(10000):
-            b = np.dot(b, self.P)
-            if (np.round(b, 8) == np.round(np.dot(b, self.P), 8)).all():
-                print(f"Number of steps: { k + 1 }")
+            b = np.dot(b, self.transition_matrix)
+            if (np.round(b, 8) == np.round(np.dot(b, self.transition_matrix), 8)).all():
+                print(f"\nb(k): { k + 1 }\n"
+                      f"{np.round(b, 8)}")
                 break
-            print(np.round(b, 7))
+            print(np.round(b, 8))
 
     def visualize(self):
 
         digraph = 'digraph finite_state_machine { size="8,8" node [shape = circle];'
 
-        for enj, j in enumerate(self.P):
+        for enj, j in enumerate(self.transition_matrix):
             for eni, i in enumerate(j):
                 if i != 0:
                     digraph += f"{enj + 1} -> {eni + 1} [label = {i}];"
@@ -32,17 +33,19 @@ class MarkovChains:
         digraph += "}"
 
         s = Source(digraph, filename="markov_chains_graph.gv", format="png")
-        s.view()
+        s.render(filename='graph')
+        # s.view()
 
 
 if __name__ == '__main__':
 
-    b0 = [0.2, 0.4, 0.1, 0.3]
-    P = [[0.2, 0.8, 0.0, 0.0],
-         [0.3, 0.2, 0.2, 0.3],
-         [0.0, 0.0, 0.2, 0.8],
-         [0.5, 0.3, 0.0, 0.2]]
+    initial_state = [0.2, 0.4, 0.1, 0.2, 0.1]
+    transition_matrix = [[0.2, 0.7, 0.0, 0.0, 0.1],
+                         [0.3, 0.2, 0.2, 0.3, 0.0],
+                         [0.0, 0.0, 0.2, 0.5, 0.3],
+                         [0.4, 0.3, 0.0, 0.2, 0.1],
+                         [0.5, 0.3, 0.0, 0.2, 0.0]]
 
-    markov_chain = MarkovChains(b0, P)
-    MarkovChains.chains(markov_chain)
+    markov_chain = MarkovChains(initial_state, transition_matrix)
+    MarkovChains.ergodic_distribution(markov_chain)
     MarkovChains.visualize(markov_chain)
